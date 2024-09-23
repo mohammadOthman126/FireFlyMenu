94566baa78bc3ed4b6452fa69deb7ed82fd237c2
mohammadOthman126 <mohhal111222@gmail.com>
2024-09-23 06:05:17 +0300

    add search 1st

---
+++ js/Beef.js
@@ -0,0 +1,34 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    const query = document.getElementById('searchInput').value.toLowerCase();
+    const items = document.querySelectorAll('.gridItem');
+
+    let hasVisibleItems = false;
+
+    items.forEach(item => {
+        const contentText = item.querySelector('h1.content').textContent.toLowerCase();
+        const priceText1 = item.querySelector('h1.price').textContent.toLowerCase();
+        const priceText2 = item.querySelector('h3.price').textContent.toLowerCase();
+
+        if (contentText.includes(query) || priceText1.includes(query) || priceText2.includes(query) ) {
+            item.style.display = ''; // عرض العنصر
+            hasVisibleItems = true;
+        } else {
+            item.style.display = 'none'; // إخفاء العنصر
+        }
+    });
+
+    if (hasVisibleItems) {
+        document.getElementById('reloadButton').style.display = 'block';
+        document.getElementById('searchButton').style.display = 'none';
+         
+    } else {
+        alert('لا توجد عناصر تطابق البحث!');
+        document.getElementById('reloadButton').style.display = 'block'; 
+        document.getElementById('searchButton').style.display = 'none';
+        // إخفاء الزر إذا لم توجد عناصر
+    }
+});
+
+document.getElementById('reloadButton').addEventListener('click', function() {
+    location.reload();
+});
